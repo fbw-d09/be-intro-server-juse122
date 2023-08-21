3d746de1a63b88a348a68994dcb85cd55d333ff4
Jules <110929073+juse122@users.noreply.github.com>
2023-08-21 15:08:44 +0200

    Add solution

--- Server.js
+++ Server.js
@@ -1,38 +1,36 @@
 // Import required modules
 const http = require('http');
-const url = require('url');
-const dotenv = require('dotenv');
 
 // Load environment variables from .env file
-dotenv.config();
+require("dotenv").config();
 
 // Initialize a counter
 let counter = 0;
 
 // Create a server instance
-const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
+const port = process.env.PORT;
 
-  // Define routes
-  if (parsedUrl.pathname === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World');
-  } else if (parsedUrl.pathname === '/now') {
-    const now = new Date();
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(`Current Date and Time: ${now}`);
-  } else if (parsedUrl.pathname === '/count') {
-    counter++;
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(`Count: ${counter}`);
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
+const server = http.createServer((req, res) => {
+    switch (req.url) {
+        case "/":
+            res.write("Hello World");
+            res.end();
+            break;
+    
+        case "/now":
+            res.write(`${ new Date().toLocaleDateString("de-DE") }, ${ new Date().toLocaleTimeString("de-DE") }`);
+            res.end();
+            break;
+
+        case "/count":
+            res.write(`${ counter }`);
+            counter++;
+            res.end();
+            break;
+    };
 });
 
 // Start the server
-const port = process.env.PORT;
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Der Server läuft auf Port ${ port }.`);
 });
